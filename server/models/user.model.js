7bcd4e313d1defc9d7709f57de703f5bd2698985
iamkuldeep07 <kuldeep45.kd@gmail.com>
2025-07-06 12:09:30 +0530

    schema done

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        public_id: {
+            type: String,
+        },
+        url: {
+            type: String,
+        },
+    },
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);
